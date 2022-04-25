85a9615b31e04c29a04fdb95675bbf78ddb1155b
Javascript <maximilian.fulterer@wickie-agentur.com>
2022-04-25 18:17:40 +0200

    1st commit

---
+++ Projekt/components.php
@@ -0,0 +1,23 @@
+<?php
+
+    function getNav() {
+    ?>
+        <nav class=navbar-main>
+        <a href="index.php">Home</a>
+        <a href="about.php">About</a>
+        <a href="contact.php">Contact</a>
+        </nav>
+        <?php
+    }
+
+
+function getFooter() {
+    ?>
+
+    <footer>
+            <footer class=footer-main>
+            <a href="siteNotice.php">Site Notice</a>
+            <a href="privacy.php">Privacy</a>
+            </footer>
+<?php
+}
